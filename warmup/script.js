068b360aeb7224ca0a1ce6fa8b2e6d43d05ba2c0
Suyeon Kim <ksy11090@naver.com>
2023-06-06 08:09:27 +0900

    momentum clone

---
+++ warmup/script.js
@@ -0,0 +1,55 @@
+// // alert("hi")
+// const player = {
+//     name: "suyeon",
+//     age: 17,
+// };
+
+// console.log(player.name);
+
+// player.bd = 1109;
+
+// const age=prompt("How old are you? "); // stops execution of the code, not pretty, very old
+
+// let a = "17";
+// console.log(typeof a);
+
+// parseInt("13");
+
+// const title = document.querySelector(".hello h1"); 
+// // css 방식으로
+// // 여러개 있음 첫번째꺼만
+// // querySelectorAll ==> 다 가져옴 어레이로
+// console.log(title); 
+// console.dir(title); // object로 표시한 element 보여줌
+
+// function handleTitleClick() {
+//     console.log("Title was clicked");
+// }
+
+// function handleMouseEnter() {
+//     // console.log("Mouse is here!")
+//     title.innerText= "Mouse is here!";
+// }
+
+// function handleMouseLeave() {
+//     // console.log("Mouse is here!")
+//     title.innerText= "Mouse is gone!";
+// }
+
+// title.addEventListener("click", handleTitleClick);
+// // click event 일어나면 handleTitleClick function execute
+// // title.onclick = handleTitleClick;
+// title.addEventListener("mouseenter", handleMouseEnter);
+
+// // window events
+// function handleWindowResize() {
+//     document.body.style.backgroundColor = "tomato";
+// }
+// window.addEventListener("resize", handleWindowResize);
+
+// // clipboard event
+// function handleWindowResize() {
+//     alert("copier!");
+// }
+// window.addEventListener("copy", handleWindowCopy);
+
